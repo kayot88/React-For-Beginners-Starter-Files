46ccc0a3b2ee63761b7f14c7afce8974f55ab882
Kayot88 <kayot88@gmail.com>
2019-02-25 13:39:38 +0200

    deploy&eject

---
+++ catch-of-the-day/build/precache-manifest.6641ba75e6aca93e0bb2495689c9a885.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "ae3f31c989cacda51217",
+    "url": "/static/css/main.c31c2fc9.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "21135faedf2b05303fb6",
+    "url": "/static/js/2.21135fae.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "ae3f31c989cacda51217",
+    "url": "/static/js/main.ae3f31c9.chunk.js"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "b1038b6d92bbcafbfb45ad4270f6829e",
+    "url": "/index.html"
+  }
+];
